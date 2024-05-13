b1f00814bb421694b2993d21f6f733a82637bede
Alex Naranjo <asnaranjom@gmail.com>
2024-05-13 10:10:20 -0600

    New changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from './app.js'
 import {consultUsers, getConnection} from './database/connection.js'
-
+console.log('Antes de cromar')
 getConnection()
+console.log('Despues de cromar')
 
-app.listen(3000)
+app.listen(1433)
